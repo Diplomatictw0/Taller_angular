1a85d978217a73320a658a6273fc457a2326e79a
Diplomatictw0 <j.cuervoo@uniandes.edu.co>
2024-04-21 15:12:24 -0500

    lista de componentes

---
+++ mynewapp/src/app/Series/Serie-list/Serie-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Serie-list',
+  templateUrl: './Serie-list.component.html',
+  styleUrls: ['./Serie-list.component.css']
+})
+export class SerieListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- mynewapp/src/app/Series/Series.module.ts
+++ mynewapp/src/app/Series/Series.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SeriesComponent } from './Series.component';
+import { SerieListComponent } from './Serie-list/Serie-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SeriesComponent]
+  declarations: [SeriesComponent, SerieListComponent],
+  exports: [SeriesComponent, SerieListComponent]
 })
 export class SeriesModule { }
