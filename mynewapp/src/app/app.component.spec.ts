899c409aa14a57fdb28cd23da632fc79bd01fb77
Diplomatictw0 <j.cuervoo@uniandes.edu.co>
2024-04-21 19:21:06 -0500

    Casi terminada, faltan mejora de vista

--- mynewapp/src/app/app.component.spec.ts
+++ mynewapp/src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mynewapp'`, () => {
+  it(`should have the 'gestion-series-tv' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mynewapp');
+    expect(app.title).toEqual('gestion-series-tv');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mynewapp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, gestion-series-tv');
   });
 });
--- mynewapp/src/app/app.module.ts
+++ mynewapp/src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+import { SerieListComponent } from './Series/Serie-list/Serie-list.component';
 import { AppComponent } from './app.component';
+import { SeriesModule } from './Series/Series.module';
+
+const routes: Routes = [
+    { path: '', component: SerieListComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        SeriesModule
+    ],
+    bootstrap: [AppComponent] // Agrega AppComponent aquí
 })
 export class AppModule { }
